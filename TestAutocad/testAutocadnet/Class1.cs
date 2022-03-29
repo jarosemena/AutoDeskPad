1fc6486ccc1dbcd10332c104b8072cf4717c9aae
Jose Jair <jjarosemena@gmail.com>
2022-03-28 22:51:39 -0500

    prueba de concempto avanzada antes de implementar la libretria de calculo de loads

--- TestAutocad/testAutocadnet/Class1.cs
+++ TestAutocad/testAutocadnet/Class1.cs
@@ -53,7 +53,87 @@ namespace testAutocadnet
                 tr.Commit();
             }
         }
-        [CommandMethod("RedefiningABlock")]
+
+
+[CommandMethod("MoveObject")]
+    public static void MoveObject()
+    {
+        // Get the current document and database
+        Document acDoc = Application.DocumentManager.MdiActiveDocument;
+        Database acCurDb = acDoc.Database;
+        // Start a transaction
+        using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+        {
+            // Open the Block table for read
+            BlockTable acBlkTbl;
+            acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
+                                               OpenMode.ForRead) as BlockTable;
+            // Open the Block table record Model space for write
+            BlockTableRecord acBlkTblRec;
+            acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
+                                                  OpenMode.ForWrite) as BlockTableRecord;
+            // Create a circle that is at 2,2 with a radius of 0.5
+            Circle acCirc = new Circle();
+            acCirc.SetDatabaseDefaults();
+            acCirc.Center = new Point3d(2, 2, 0);
+            acCirc.Radius = 0.5;
+            // Create a matrix and move the circle using a vector from (0,0,0) to (2,0,0)
+            Point3d acPt3d = new Point3d(0, 0, 0);
+            Vector3d acVec3d = acPt3d.GetVectorTo(new Point3d(2, 0, 0));
+            acCirc.TransformBy(Matrix3d.Displacement(acVec3d));
+            // Add the new object to the block table record and the transaction
+            acBlkTblRec.AppendEntity(acCirc);
+            acTrans.AddNewlyCreatedDBObject(acCirc, true);
+            // Save the new objects to the database
+            acTrans.Commit();
+        }
+    }
+
+
+    [CommandMethod("RotateObject")]
+        public static void RotateObject()
+        {
+            // Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the Block table for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
+                                                   OpenMode.ForRead) as BlockTable;
+                // Open the Block table record Model space for write
+                BlockTableRecord acBlkTblRec;
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
+                                                      OpenMode.ForWrite) as BlockTableRecord;
+                // Create a lightweight polyline
+                Polyline acPoly = new Polyline();
+                acPoly.SetDatabaseDefaults();
+                acPoly.AddVertexAt(0, new Point2d(1, 2), 0, 0, 0);
+                acPoly.AddVertexAt(1, new Point2d(1, 3), 0, 0, 0);
+                acPoly.AddVertexAt(2, new Point2d(2, 3), 0, 0, 0);
+                acPoly.AddVertexAt(3, new Point2d(3, 3), 0, 0, 0);
+                acPoly.AddVertexAt(4, new Point2d(4, 4), 0, 0, 0);
+                acPoly.AddVertexAt(5, new Point2d(4, 2), 0, 0, 0);
+                // Close the polyline
+                acPoly.Closed = true;
+                Matrix3d curUCSMatrix = acDoc.Editor.CurrentUserCoordinateSystem;
+                CoordinateSystem3d curUCS = curUCSMatrix.CoordinateSystem3d;
+                // Rotate the polyline 45 degrees, around the Z-axis of the current UCS
+                // using a base point of (4,4.25,0)
+                acPoly.TransformBy(Matrix3d.Rotation(0.7854,
+                                                           curUCS.Zaxis,
+                                                           new Point3d(4, 4.25, 0)));
+                // Add the new object to the block table record and the transaction
+                acBlkTblRec.AppendEntity(acPoly);
+                acTrans.AddNewlyCreatedDBObject(acPoly, true);
+                // Save the new objects to the database
+                acTrans.Commit();
+            }
+        }
+
+        [CommandMethod("changeABlock")]
         public void RedefiningABlock()
         {
             // Get the current database and start a transaction
@@ -87,7 +167,10 @@ namespace testAutocadnet
                             Polyline acPol = dbObj as Polyline;
 
                             acPol.UpgradeOpen();
-                            //acPol.;
+                            acPol.EndPoint = new Point3d(0,0,0);
+                            acPol.StartPoint = new Point3d(0, 0, 0);
+                            acPol.
+                            
                         }
                     }
 
@@ -105,6 +188,39 @@ namespace testAutocadnet
                 acTrans.Commit();
 
                 // Dispose of the transaction
+            }
+        }
+
+        [CommandMethod("Matrix_ScaleRotationMovement")]
+        public static void Matrix_ScaleRotationMovement()
+        {
+            Editor ed = MgdAcApplication.DocumentManager.MdiActiveDocument.Editor;
+            Database db = HostApplicationServices.WorkingDatabase;
+            try
+            {
+                ObjectId faceId = CreateFaceOnXYPlaneOfWCS();
+
+                Transaction trans = faceId.Database.TransactionManager.StartTransaction();
+                using (trans)
+                {
+                    DBObject obj = (DBObject)trans.GetObject(faceId, OpenMode.ForWrite);
+                    if (obj is Face)
+                    {
+                        Face castObj = (Face)obj;
+
+                        //Modify the object below ...
+                        Matrix3d scaleMat = Matrix3d.Scaling(2, Point3d.Origin);
+                        Matrix3d rotationMat = Matrix3d.Rotation(Math.PI / 4, Vector3d.ZAxis, Point3d.Origin);
+                        Matrix3d movementMat = Matrix3d.Displacement(new Vector3d(3, 3, 0));
+                        castObj.TransformBy(scaleMat.PreMultiplyBy(rotationMat).PreMultiplyBy(movementMat));
+                    }
+
+                    trans.Commit();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                ed.WriteMessage(ex.ToString());
             }
         }
         public ElementModel UpdateElement( ElementModel Element)
