3598526376660e98ea687109c753a354bc6511d3
Jose Jair <jjarosemena@gmail.com>
2022-04-23 11:47:57 -0500

    Jose Arosemena 20220423 instalation of the new li

---
+++ TestAutocad/testAutocadnet/Materials_List.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FEALiTE2D.Materials;
+
+namespace testAutocadnet
+{
+    static class Materials_List
+    {
+        public static IMaterial steel { get { return new GenericIsotropicMaterial() { E = 30E6, U = 0.2, Label = "Steel", Alpha = 0.000012, Gama = 39885, MaterialType = MaterialType.Steel }; } }
+    }
+}
